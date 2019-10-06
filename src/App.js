1e4c49c35c871007b2e1badfc63dfc68c7496ccb
unknown <civoremon@gmail.com>
2019-10-06 18:10:50 -0400

    moved files to root

---
+++ src/App.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
+
+import Navigation from './components/navbar';
+import ServicesPage from './components/services';
+import AboutUs from './components/aboutus';
+import ContactUs from './components/contact';
+
+const URL = `http://res.cloudinary.com/ppscreens/image/list/ppsr.json`;
+
+class App extends Component {
+  state = {
+    collapsedID: '',
+    imageArray: [],
+    senderEmail: '',
+    senderName: '',
+    senderMessage: '',
+    error: {
+      name: '',
+      email: '',
+      message: ''
+    },
+    insufficientInfo: false
+  }
+
+  componentDidMount() {
+    axios
+      .get(URL)
+      .then(res => {
+        this.setState({
+          imageArray: res.data.resources
+        });
+      })
+      .catch(err => {
+        console.log("ERROR --> " + err);
+      })
+  }
+
+  toggleAccordion = collapsedID => () => 
+    this.setState(prevState => ({
+      collapsedID: prevState.collapsedID !== collapsedID ? collapsedID : ''
+  }))
+
+  handleInputChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  clearInputs = (event) => {
+    this.setState({
+      senderEmail: '',
+      senderName: '',
+      senderMessage: '',
+      error: {},
+      insufficientInfo: false
+    })
+  }
+
+  setErrorMessages = (errors) => {
+    this.setState({
+      error: errors
+    })
+  } 
+
+  toggleMissingInfoMessage = (event) => {
+    this.setState({
+      insufficientInfo: true
+    })
+  }
+
+  render() {
+
+    return (
+      <div style={{height: '2000px'}}>
+        <Navigation 
+          imageArray={this.state.imageArray}
+        />
+        <div className='callToActionSpacer' />
+        <AboutUs />
+        <ServicesPage 
+          collapsedID={this.state.collapsedID}
+          toggleAccordion={this.toggleAccordion}
+        />
+        <ContactUs 
+          handleInputChange={this.handleInputChange}
+          setErrorMessages={this.setErrorMessages}
+          clearInputs={this.clearInputs}
+          toggleMissingInfoMessage={this.toggleMissingInfoMessage}
+          senderEmail={this.state.senderEmail}
+          senderName={this.state.senderName}
+          senderMessage={this.state.senderMessage}
+          senderError={this.state.error}
+          insufficientInfo={this.state.insufficientInfo}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,125 @@
+import React, { useState } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import styled from 'styled-components';
+
+// import logoImg from './images/logo/logo_lg_alt.png';
+import logoImgWhite from './images/logo/logo_lg_alt_white.png';
+import Gallery from './gallery';
+
+/*
+  colors
+
+  blue1: #163e98
+  blue2: #12317a
+
+*/
+
+const NavLinkColor = styled.span`
+	color: #eeeeee;
+  font-weight: bold;
+  
+  font-family: 'Raleway', sans-serif;
+
+	:hover {
+    color: #c8c8c8;
+    text-decoration: underline;
+	}
+`;
+
+const ActionDiv = styled.div `
+  background-color: #163e98;
+  color: whitesmoke;
+  position: absolute;
+  top: 4.3rem;
+  width: 100%;
+`
+
+const PhoneNumberA = styled.a `
+  font-size: 1.6rem;
+  color: #fff;
+
+  @media (min-width: 992px) {
+    display: block;
+  }
+
+  @media (max-width: 991px) {
+    display: none;
+  }
+
+  :hover {
+    text-decoration: underline;
+    color: #fff;
+  }
+
+`
+const ActionTextSpan = styled.span `
+  font-size: 1rem;
+  color: #fff;
+
+  font-family: 'Roboto', sans-serif;
+
+  @media (max-width: 991px) {
+    display: none;
+  }
+
+  @media (min-width: 992px) {
+    display: block;
+  }
+`
+
+const Navigationbar = props => {
+  const { imageArray } = props;
+  const [lgGallery, setGallery] = useState(false);
+
+  return (
+    <div>
+      
+      <Navbar fixed='top' expand='sm' style={{backgroundColor: '#1759aa'}}>
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src={logoImgWhite}
+              alt={'PPSR'}
+              style={{maxWidth: '120px', height: 'auto', marginRight: '30px'}}
+            />
+          </Navbar.Brand>
+          <div style={{display: 'flex', justifyContent: 'center', alignItems: 'baseline'}}>
+            <ActionTextSpan style={{ marginRight: '10px'}}>Call </ActionTextSpan>
+            <PhoneNumberA href='tel:4078008116'>407-800-8116</PhoneNumberA>
+            <ActionTextSpan style={{ marginLeft: '10px'}}> for a Free Estimate!</ActionTextSpan>
+          </div>
+          <Navbar.Toggle className='hamburger-custom' aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto" />
+            <Nav className="navbar-custom">
+              <Nav.Link href="#about"><NavLinkColor>About</NavLinkColor></Nav.Link>
+              <Nav.Link href="#services"><NavLinkColor>Services</NavLinkColor></Nav.Link>
+              <Nav.Link href="#financing"><NavLinkColor>Financing</NavLinkColor></Nav.Link>
+              <Nav.Link href="#gallery"><NavLinkColor onClick={() => setGallery(true)}>Gallery</NavLinkColor></Nav.Link>
+              <Nav.Link href="#contact"><NavLinkColor>Contact</NavLinkColor></Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <ActionDiv className='callToAction'>
+        <Container>
+          <div style={{ maxWidth: '100%',display: 'flex', justifyContent: 'space-around', alignItems: 'center',height: '3rem'}}>
+            <span style={{fontSize: '1.4rem'}}> Call <a href='tel:4078008116' style={{color: '#fff'}}>407-800-8116</a> for a Free Estimate</span>
+          </div>
+        </Container>
+      </ActionDiv>
+
+      <Gallery 
+        imageArray={imageArray} 
+        lgGallery={lgGallery}
+        setGallery={setGallery}
+      />
+      
+    </div>
+  )
+}
+
+export default Navigationbar;

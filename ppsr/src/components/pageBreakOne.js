ae99447413e20ad0fb98210e0193d1408b094f14
civoremo <civormeon@gmail.com>
2019-11-29 19:47:48 -0500

    page breaks have parallax scrolling background

--- ppsr/src/components/pageBreakOne.js
+++ ppsr/src/components/pageBreakOne.js
@@ -1,16 +1,37 @@
 import React, { Component } from "react";
 import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 import styled from "styled-components";
+
+import pageBreakImageOne from "./images/accordionImages/pool2.jpg";
 
 const BackgroundDiv = styled.div`
 	background-color: yellow;
-	min-height: 250px;
+	height: 450px;
+	background-image: url(${pageBreakImageOne});
+	background-size: cover;
+	clip-path: polygon(0 0, 100% 10%, 100% 100%, 0 90%);
+	background-attachment: fixed;
+	background-position: center;
+`;
+
+const PageBreakContentOne = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 450px;
 `;
 
 const PageBreakOne = props => {
 	return (
 		<BackgroundDiv>
-			<Container>First page break with INFO</Container>
+			<Container>
+				<PageBreakContentOne>
+					<div style={{ fontSize: "2rem", fontWeight: "bold", color: "white" }}>
+						Some Text can go here
+					</div>
+				</PageBreakContentOne>
+			</Container>
 		</BackgroundDiv>
 	);
 };
--- ppsr/src/components/pageBreakTwo.js
+++ ppsr/src/components/pageBreakTwo.js
@@ -2,15 +2,35 @@ import React, { Component } from "react";
 import Container from "react-bootstrap/Container";
 import styled from "styled-components";
 
+import pageBreakImageTwo from "./images/accordionImages/pool3.jpg";
+
 const BackgroundTwoDiv = styled.div`
 	background-color: green;
-	min-height: 250px;
+	min-height: 450px;
+	background-image: url(${pageBreakImageTwo});
+	background-size: cover;
+	clip-path: polygon(0 0, 100% 10%, 100% 100%, 0 90%);
+	background-attachment: fixed;
+	background-position: center;
+`;
+
+const PageBreakContentTwo = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 450px;
 `;
 
 const PageBreakTwo = props => {
 	return (
 		<BackgroundTwoDiv>
-			<Container>Second Page break with INFO</Container>
+			<Container>
+				<PageBreakContentTwo>
+					<div style={{ fontSize: "2rem", fontWeight: "bold", color: "#007bff" }}>
+						Some Text can go here
+					</div>
+				</PageBreakContentTwo>
+			</Container>
 		</BackgroundTwoDiv>
 	);
 };
